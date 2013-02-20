78ec0511129134f4786ba74d9692d64f2c25e8e1
CoryFoy <github@cornetdesign.com>
2013-02-20 07:32:10 -0500

    Corrected styles for headers

--- lib/rtf/converters/html.rb
+++ lib/rtf/converters/html.rb
@@ -74,12 +74,6 @@ module RTF::Converters
           when :h6
             style.font_size = 12
             style.bold = true
-          when :h5
-            style.font_size = 16
-            style.bold = true
-          when :h6
-            style.font_size = 12
-            style.bold = true
           end
         end
       end
